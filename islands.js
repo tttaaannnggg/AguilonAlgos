2b7862a20116e62617699dc23a89791e5bb3612c
tang <eytang8@gmail.com>
2019-06-21 12:34:52 -0700

    finished islands. could use refactored logic

--- islands.js
+++ islands.js
@@ -28,14 +28,25 @@
  * Output: 3
 */
 
-const map = [
-  ['0','0','0','0','0'],
-  ['0','0','0','0','0'],
-  ['0','0','0','0','0']
-];
-let number = 1;
+function countIslands(grid){
+  const map = [];
+  let count = 0;
+  for(let i = 0; i < grid.length; i++){
+    for(let j = 0; j < grid[i].length; j++){
+      if (!map[i]){
+        map[i] = [];
+      }
+      if (map[i][j] !== undefined || grid[i][j] === '0'){
+        continue;
+      }
+      mapOneIsland(i,j,grid,count, map);
+      count++;
+    }
+  }
+  return count
+}
 
-function mapOneIsland(x,y,grid, name){
+function mapOneIsland(x,y,grid, name, map){
   if(grid[x] === undefined || grid[x][y] === undefined || grid[x][y] === '0'){
     return
   }
@@ -53,18 +64,15 @@ function mapOneIsland(x,y,grid, name){
     [x,y+1]
   ]
   for (let i = 0; i < neighbors.length; i++){
-    mapOneIsland(neighbors[i][0],neighbors[i][1],grid,name);
+    mapOneIsland(neighbors[i][0],neighbors[i][1],grid,name, map);
   }
-  return map;
 }
 
 const landMap = [
-  ['1','0','0','0','0'],
-  ['1','0','0','1','0'],
-  ['1','0','0','1','0']
+  ['1','0','0','0','0', '1'],
+  ['1','0','0','1','1', '1'],
+  ['1','1','1','1','0']
 ];
 
 console.log(landMap);
-console.log(mapOneIsland(0,0,landMap,'a'));
-
-console.log(mapOneIsland(1,3,landMap,'b'));
+console.log(countIslands(landMap));
