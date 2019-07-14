00d373c94194130edf8e1170a1a9e352bdfc1e03
tang <eytang8@gmail.com>
2019-07-14 12:31:17 -0700

    optimized RSQ

--- range-sum-query.js
+++ range-sum-query.js
@@ -7,28 +7,19 @@
 // within a given range (i,j), inclusive
 //
 
-const testVals = [0,1,2,3,4,5];
 
 const Range = function(nums){
-  this.cache = [[]];
-  let runningSum = 0;
+  this.cache = [0];
   for (let i = 0; i < nums.length; i++){
-    runningSum += nums[i];
-    this.cache[0].push(runningSum);
-  }
-  for (let i = 0; i < nums.length-1; i++){
-    const slice = new Array(i+1);
-    for (let j = i+1; j < nums.length; j++){
-      slice.push(this.cache[0][j] - this.cache[0][i])
-    }
-    this.cache.push(slice);
+    this.cache.push(this.cache[i]+nums[i]);
   }
 }
 
 Range.prototype.sumRange = function(i,j){
-  return this.cache[i][j];
+  return this.cache[j+1]-this.cache[i];
 }
 
+const testVals = [0,1,2,3,4,5];
 const test = new Range(testVals);
-console.log(test.cache)
-console.log(test.sumRange(1,3));
+console.log(test.sumRange(1,3), 'should be 6');
+console.log(test.sumRange(1,1), 'should be 1');
