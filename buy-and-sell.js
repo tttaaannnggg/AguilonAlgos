2bea137f3d1dc0139868c67f8dd6d41d012f9cf8
Tang <eytang8@gmail.com>
2019-07-28 19:07:56 -0700

    finished buy and sell stock

---
+++ buy-and-sell.js
@@ -0,0 +1,21 @@
+/*
+ * we've got a list of stock prices over the course of some interval
+ * how do we figure out what the maximum possible profit over the course of a day is?
+ * this can be done in O(n) time
+ */
+
+const maxProfit = (prices)=>{
+      let profit = 0
+      let lowestVal = prices[0];
+      for (let i = 0; i < prices.length; i++){
+                const newProfit = prices[i]-lowestVal;
+                if(newProfit > profit){
+                              profit = newProfit
+                          }
+                if (prices[i]<lowestVal){
+                            lowestVal = prices[i];
+                          }
+            }
+      return profit
+}
+
